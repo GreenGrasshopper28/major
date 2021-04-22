85bb4bacda1bb6b21a5561af7029a68d6d40e302
GreenGrasshopper28 <80503370+GreenGrasshopper28@users.noreply.github.com>
2021-04-22 16:24:12 +0530

    Update app (2).py

--- app (2).py
+++ app (2).py
@@ -4,22 +4,27 @@ import numpy as np
 from PIL import Image # Strreamlit works with PIL library very easily for Images
 import cv2
 from keras.applications.mobilenet_v2 import preprocess_input,decode_predictions
+
+@st.cache()
+def load_model():
+  model = MobileNetV2()
+  return model
+
+
 st.title("Image-Classifier: Cats and Dogs")
 upload = st.sidebar.file_uploader(label='Upload the Image')
-
-
-
 if upload is not None:
   file_bytes = np.asarray(bytearray(upload.read()), dtype=np.uint8)
   opencv_image = cv2.imdecode(file_bytes, 1)
   opencv_image = cv2.cvtColor(opencv_image,cv2.COLOR_BGR2RGB) # Color from BGR to RGB
   img = Image.open(upload)
   st.image(img,caption='Uploaded Image',width=300)
-  model_new = MobileNetV2()
+  model = load_model()
+  #model_new = MobileNetV2()
   x = cv2.resize(opencv_image,(224,224))
   x = np.expand_dims(x,axis=0)
   x = preprocess_input(x)
-  y = model_new.predict(x)
+  y = model.predict(x)
   if st.sidebar.button('PREDICT'):
     st.sidebar.write('Result...')
     label = decode_predictions(y)
